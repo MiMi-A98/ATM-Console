4180fefd3580e1bf805e1ee235348cc4c742985e
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-04-16 20:57:58 +0300

    implemented logic for blocked card

--- src/main/java/MiMiA98/atm/Card.java
+++ src/main/java/MiMiA98/atm/Card.java
@@ -4,11 +4,15 @@ public class Card {
 
     private int cardNumber;
     private String pin;
+    private boolean isLocked;
+
     private BankAccount bankAccount;
+
 
     public Card(int cardNumber, String pin, BankAccount bankAccount) {
         this.cardNumber = cardNumber;
         this.pin = pin;
+        this.isLocked = false;
         this.bankAccount = bankAccount;
         bankAccount.addCard(this);
     }
@@ -21,15 +25,27 @@ public class Card {
         return pin;
     }
 
-    public BankAccount getBankAccount() {
-        return bankAccount;
+    public void setPin(String newPin) {
+        if (isLocked) {
+            throw new IllegalStateException("Card is blocked!");
+        } else {
+            if (newPin.matches("\\d{4}")) {
+                this.pin = newPin;
+            } else {
+                throw new IllegalArgumentException("New pin doesn't fulfill the requirements!");
+            }
+        }
     }
 
-    public void changePin(String newPin) {
-        if (newPin.matches("\\d{4}")) {
-            this.pin = newPin;
-        } else {
-            throw new IllegalArgumentException("New pin doesn't fulfill the requirements!");
-        }
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public BankAccount getBankAccount() {
+        return bankAccount;
     }
 }
---
+++ src/test/java/MiMiA98/atm/AutomatedTellerMachineTest.java
@@ -0,0 +1,40 @@
+package MiMiA98.atm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AutomatedTellerMachineTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void displayMainMenu() {
+    }
+
+    @Test
+    void viewBalance() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void changePin() {
+    }
+}

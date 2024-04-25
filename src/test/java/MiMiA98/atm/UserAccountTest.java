c1bab9286d2e90397b3ad6805b12fb1fcc191a30
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-04-25 19:38:57 +0300

    tested logic for put card in map

--- src/test/java/MiMiA98/atm/UserAccountTest.java
+++ src/test/java/MiMiA98/atm/UserAccountTest.java
@@ -4,20 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserAccountTest {
     UserAccount userAccount;
 
     @BeforeEach
     void setUp() {
-        userAccount = new UserAccount(1, "John Smith");
+        userAccount = new UserAccount("1", "John Smith");
     }
 
     @Test
     void addBankAccount_withBankAccount_increasesUserBankAccountsCollectionSize() {
         BankAccount bankAccount = Mockito.mock(BankAccount.class);
-        userAccount.addBankAccount(bankAccount);
-        assertEquals(1, userAccount.bankAccounts.size());
+        userAccount.addBankAccount(1, bankAccount);
+        assertEquals(1, userAccount.getBankAccounts().size());
     }
 }

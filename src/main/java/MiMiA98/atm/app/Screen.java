b98e3e316eddd710b752aba0a7bb280b5d7fb5b6
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-05-13 19:14:19 +0300

    created Screen class

---
+++ src/main/java/MiMiA98/atm/app/Screen.java
@@ -0,0 +1,25 @@
+package MiMiA98.atm.app;
+
+import java.util.Scanner;
+
+public class Screen {
+
+    public static void display(String message) {
+        System.out.println(message);
+    }
+
+    public static int getInputInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static String getInputString() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static double getInputDouble() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+}

7bc51af0ff39b7370cad67c9306a054ff36795fc
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-08-15 16:13:31 +0300

    added initializeData and setPersistence methods

--- src/main/java/MiMiA98/atm/app/Main.java
+++ src/main/java/MiMiA98/atm/app/Main.java
@@ -1,6 +1,7 @@
 package MiMiA98.atm.app;
 
 
+import MiMiA98.atm.dao.UtilDAO;
 import MiMiA98.atm.entity.Card;
 import MiMiA98.atm.service.*;
 
@@ -12,6 +13,8 @@ public class Main {
         AutomatedTellerMachine atm = new AutomatedTellerMachine();
 
         try {
+            setPersistenceUnit("h2_database");
+
             initializeData();
 
             Card card = atm.chooseCard();
@@ -29,25 +32,30 @@ public class Main {
     }
 
     public static void initializeData() {
+
         CardService cardService = new CardService();
         UserAccountService userAccountService = new UserAccountService();
         CheckingAccountService checkingAccountService = new CheckingAccountService();
         SavingsService savingsService = new SavingsService();
         FixedTermService fixedTermService = new FixedTermService();
 
-        userAccountService.createUserAccount("User1", "Ana B.");
-        checkingAccountService.createCheckingAccount("Checking1", "USD", "User1");
-        cardService.createCard("Card1", "1234", "Checking1");
-        savingsService.createSavingsAccount("Savings1", "RON", 1, "User1");
-        fixedTermService.createFixedTermAccount("FixedTerm1", "User1", "RON", 1, BigDecimal.valueOf(100));
+        userAccountService.createUserAccount("1001", "Ana B.");
+        checkingAccountService.createCheckingAccount("ROC1111", "USD", "1001");
+        cardService.createCard("1234567890", "1234", "ROC1111");
+        savingsService.createSavingsAccount("ROS1111", "RON", 1, "1001");
+        fixedTermService.createFixedTermAccount("ROFT1111", "1001", "RON", 1, BigDecimal.valueOf(100));
 
-        userAccountService.createUserAccount("User2", "Bianca C.");
-        checkingAccountService.createCheckingAccount("Checking2", "USD", "User2");
-        cardService.createCard("Card2", "1234", "Checking2");
+        userAccountService.createUserAccount("1002", "Bianca C.");
+        checkingAccountService.createCheckingAccount("ROC1112", "USD", "1002");
+        cardService.createCard("2345678901", "1234", "ROC1112");
 
-        userAccountService.createUserAccount("User3", "Cristina D.");
-        checkingAccountService.createCheckingAccount("Checking3", "USD", "User3");
-        cardService.createCard("Card3", "1234", "Checking3");
-        savingsService.createSavingsAccount("Savings3", "RON", 1, "User3");
+        userAccountService.createUserAccount("1003", "Cristina D.");
+        checkingAccountService.createCheckingAccount("ROC1113", "USD", "1003");
+        cardService.createCard("3456789012", "1234", "ROC1113");
+        savingsService.createSavingsAccount("ROS1112", "RON", 1, "1003");
+    }
+
+    public static void setPersistenceUnit(String persistenceUnitName) {
+        UtilDAO.setPersistenceUnit(persistenceUnitName);
     }
 }

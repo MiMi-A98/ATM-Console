887ea75c19dd58a180951cf03446ca6a8e53a219
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-08-24 20:52:31 +0300

    changed location of initializeDemoData method

--- src/main/java/MiMiA98/atm/app/Main.java
+++ src/main/java/MiMiA98/atm/app/Main.java
@@ -3,9 +3,6 @@ package MiMiA98.atm.app;
 
 import MiMiA98.atm.dao.UtilDAO;
 import MiMiA98.atm.entity.Card;
-import MiMiA98.atm.service.*;
-
-import java.math.BigDecimal;
 
 public class Main {
 
@@ -15,8 +12,6 @@ public class Main {
         try {
 
             UtilDAO.initializePersistenceUnit();
-
-            initializeData();
 
             Card card = atm.chooseCard();
             atm.login(card);
@@ -30,29 +25,5 @@ public class Main {
             e.printStackTrace();
         }
 
-    }
-
-    public static void initializeData() {
-
-        CardService cardService = new CardService();
-        UserAccountService userAccountService = new UserAccountService();
-        CheckingAccountService checkingAccountService = new CheckingAccountService();
-        SavingsService savingsService = new SavingsService();
-        FixedTermService fixedTermService = new FixedTermService();
-
-        userAccountService.createUserAccount("1001", "Ana B.");
-        checkingAccountService.createCheckingAccount("ROC1111", "USD", "1001");
-        cardService.createCard("1234567890", "1234", "ROC1111");
-        savingsService.createSavingsAccount("ROS1111", "RON", 1, "1001");
-        fixedTermService.createFixedTermAccount("ROFT1111", "1001", "RON", 1, BigDecimal.valueOf(100));
-
-        userAccountService.createUserAccount("1002", "Bianca C.");
-        checkingAccountService.createCheckingAccount("ROC1112", "USD", "1002");
-        cardService.createCard("2345678901", "1234", "ROC1112");
-
-        userAccountService.createUserAccount("1003", "Cristina D.");
-        checkingAccountService.createCheckingAccount("ROC1113", "USD", "1003");
-        cardService.createCard("3456789012", "1234", "ROC1113");
-        savingsService.createSavingsAccount("ROS1112", "RON", 1, "1003");
     }
 }
--- src/main/java/MiMiA98/atm/dao/UtilDAO.java
+++ src/main/java/MiMiA98/atm/dao/UtilDAO.java
@@ -1,5 +1,6 @@
 package MiMiA98.atm.dao;
 
+import MiMiA98.atm.service.*;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.h2.tools.Server;
@@ -7,6 +8,7 @@ import org.h2.tools.Server;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,6 +41,7 @@ public class UtilDAO {
             case "h2_database" -> {
                 entityManagerFactory = Persistence.createEntityManagerFactory(persistenceName);
                 startH2Server();
+                initializeDemoData();
             }
             case "mySQL_database" -> {
                 Map<String, String> configOverride = new HashMap<>();
@@ -58,5 +61,29 @@ public class UtilDAO {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void initializeDemoData() {
+
+        CardService cardService = new CardService();
+        UserAccountService userAccountService = new UserAccountService();
+        CheckingAccountService checkingAccountService = new CheckingAccountService();
+        SavingsService savingsService = new SavingsService();
+        FixedTermService fixedTermService = new FixedTermService();
+
+        userAccountService.createUserAccount("1001", "Ana B.");
+        checkingAccountService.createCheckingAccount("ROC1111", "USD", "1001");
+        cardService.createCard("1234567890", "1234", "ROC1111");
+        savingsService.createSavingsAccount("ROS1111", "RON", 1, "1001");
+        fixedTermService.createFixedTermAccount("ROFT1111", "1001", "RON", 1, BigDecimal.valueOf(100));
+
+        userAccountService.createUserAccount("1002", "Bianca C.");
+        checkingAccountService.createCheckingAccount("ROC1112", "USD", "1002");
+        cardService.createCard("2345678901", "1234", "ROC1112");
+
+        userAccountService.createUserAccount("1003", "Cristina D.");
+        checkingAccountService.createCheckingAccount("ROC1113", "USD", "1003");
+        cardService.createCard("3456789012", "1234", "ROC1113");
+        savingsService.createSavingsAccount("ROS1112", "RON", 1, "1003");
     }
 }

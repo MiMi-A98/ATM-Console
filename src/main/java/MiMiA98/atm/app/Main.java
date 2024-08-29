3596db1de9072265cd415775261ec42d5b051745
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2024-08-29 17:10:18 +0300

    added setting to format the column names in database to follow the same writing style as in the project

--- src/main/java/MiMiA98/atm/app/Main.java
+++ src/main/java/MiMiA98/atm/app/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         AutomatedTellerMachine atm = new AutomatedTellerMachine();
-
         try {
 
             UtilDAO.initializePersistenceUnit();
